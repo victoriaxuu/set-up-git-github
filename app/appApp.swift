a1450d678a0c90ee0e01cd24c58858f4480421a9
victoria <happyfunnyvicky@gmail.com>
2022-06-14 12:24:22 -0500

    Initial Commit

---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Victoria Xu on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
